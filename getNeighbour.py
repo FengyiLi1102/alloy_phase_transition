bc3324aa7cdb2778d09c49af99de04f2dae931d3
Fengyi Li <carl.yifeng.li@outlook.com>
2020-01-14 00:28:51 +0000

    Update

--- getNeighbour.py
+++ getNeighbour.py
@@ -4,18 +4,19 @@ import numpy as np
 
 def getNeighbour (size, ix1, iy1, d12):
     """
+    Description:
     GETNEIGHBOUR returns the position of a neighbouring atom.
 
     Input arguments:
-    -> size:              The size of the simulation box                        int
-    -> ix1:               X coordinate of first atom                            int
-    -> iy1:               Y coordinate of first atom                            int
-    -> d12:               Direction of second atom relative to first
-                          1: above; 2: down; 3: left; 4: right                  int
+    -> size:                  The size of the simulation box                        int
+    -> ix1:                   X coordinate of first atom                            int
+    -> iy1:                   Y coordinate of first atom                            int
+    -> d12:                   Direction of second atom relative to first
+                              1: above; 2: down; 3: left; 4: right                  int
 
     Output arguments:
-    -> ix2:               X coordinate of second atom                           int
-    -> iy2:               Y coordinate of second atom                           int
+    -> ix2:                   X coordinate of second atom                           int
+    -> iy2:                   Y coordinate of second atom                           int
     """
     #
     d12 = np.random.randint(1, 5, 1, dtype='int')
@@ -27,10 +28,17 @@ def getNeighbour (size, ix1, iy1, d12):
     return ix2, iy2
 
 
-def periodic_check(x, y):
+def periodic_check(x, y, size):
     """
 
     """
 
-    if x == -1:
+    if x > (size - 1):
+        x = 0
+    
+    else x < 0:
+        x = size - 1
+    
+    if y > 
+
         
